61697543d8d4fe2b21411c3c1fd3fade55ab27c7
shashi <shashidharavtc@gmail.com>
2019-06-28 23:15:51 +0530

    Added Earnings header file

---
+++ src/includes/earnings.h
@@ -0,0 +1,40 @@
+#ifndef __EARNINGS_H__
+#define __EARNINGS_H__
+
+#define MAX_OTHE_EARNINGS 10
+
+struct date {
+    uint32_t day;
+    uint32_t month;
+    uint32_t year;
+};
+
+typedef struct _salary {
+    uint64_t amount;
+    struct date sDate;
+}salary;
+
+typedef struct _bonus {
+    uint64_t amount;
+    struct date sDate;
+}bonus;
+
+typedef struct _othEarns {
+    char *src;
+    uint64_t amount;
+    struct date sDate;
+}othEarns;
+
+
+/**
+  * s : salary earned
+  * b : bonus earned
+  * o : other earnings
+  */
+typedef struct _earnings {
+    salary s;
+    bonus b;
+    othEarns o[MAX_OTHE_EARNINGS];
+}earnings;
+
+#endif
