ec68804c9a40cabe3a23394d924e263ef42b5acb
Varsha A N <varshaa@varshaa-mac.local>
2019-06-29 13:45:24 +0530

    Implemented saving handler

--- src/includes/savings.h
+++ src/includes/savings.h
@@ -3,8 +3,7 @@
 
 typedef struct _savings {
     char *scheme;
-    uint64_t yearlyInvested;
-    uint64_t monthlyInvested;
+    uint64_t investment;
     uint64_t totalInvestments;
     int interestEarned;
     Date date;
--- src/savings/wmSavings.c
+++ src/savings/wmSavings.c
@@ -5,15 +5,45 @@
 #include <string.h>
 #include "savings.h"
 #include "misc.h"
+#define MAX_SCHEME_LEN 100
 
-int addSavings(Savings *s, char *scheme, uint64_t monthly_invested)
+int addSavings(Savings *s, char *scheme, uint64_t investment)
 {
     int err = 0;
     WMC_PTR(s);
     WMC_PTR(scheme);
     ASSIGN_STR(s->scheme, scheme);
-    o->monthly_invested = monthly_invested;
+    o->investment = investment;
     fillDate(&s->date);
 Finally:
     return err;
 }
+
+int fetchSavingsFromCli(Savings *s)
+{
+    char scheme[MAX_SCHEME_LEN];
+    char investment[10];
+    printf("Enter saving scheme");
+    printf("###################################\n");
+    printf("Scheme Name:");
+    if (fgets(scheme, 100, stdin) == NULL)
+        perror("Scheme name");
+    else
+        ASSIGN_STR(s->scheme, scheme);
+
+    printf("Investment:");
+    if (fgets(investment, 100, stdin) == NULL)
+        perror("investment");
+    else
+        s->investment = atoi(investment);
+    return s->investment;
+}
+
+addSavings(Savings *s)
+{
+    Printf("Adding Savings\n");
+    fetchSavingsFromCli(s);
+
+}
+
+
